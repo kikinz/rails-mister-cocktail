18df8ef9ed285c3ac4d3f3680248bde08018b03c
Kirsten Judd <kirstensophie@gmail.com>
2017-06-16 17:53:31 +0800

    Updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  mount Attachinary::Engine => "/attachinary"
   resources :cocktails do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
   root to: "cocktails#index"
-  mount Attachinary::Engine => "/attachinary"
 end
